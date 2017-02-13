6397619906ecaff0be0a4153ef2ec810c61ab232
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-02-13 14:46:06 +0100

    Begin with a mere main.c file

---
+++ main.c
@@ -0,0 +1,18 @@
+/**
+ * CPE-TP3 : Jeu de la patate chaude
+ *
+ * @file main.c
+ * @brief      Implement a mere game of hot potato with a player struct array
+ * @author     Nathanaël SPRIET
+ * @date       13/02/2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "hotPotato.h"
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
